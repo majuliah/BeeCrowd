ae895028ca2cea2bdcebfcfb6c14810ae3cd049e
Majulia <majulialcarvalho@gmail.com>
2022-03-02 15:59:25 -0300

    Repositorie for exercices

---
+++ begginer/SimpleSum-1002/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace SimpleSum_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum soma = new Sum();
+            WriteLine($"Enter with three numbers: ");
+            string[] numbers = ReadLine().Split(' ');
+            
+            soma.Number1 = int.Parse(numbers[0]);
+            soma.Number2 = int.Parse(numbers[1]);
+            soma.Number3 = int.Parse(numbers[2]);
+
+            int total = (soma.AddingNumbers(soma.Number1, soma.Number2, soma.Number3));
+            WriteLine($"O resultado é: {total}");
+            
+        }
+    }
+}
---
+++ begginer/SimpleSum-1002/Sum.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleSum_1002
+{
+    public class Sum
+    {
+        public int Number1, Number2, Number3;
+
+        public Sum()
+        {
+        }
+
+        public int AddingNumbers(int number1, int number2, int number3)
+        {
+            this.Number1 = number1;
+            this.Number2 = number2;
+            this.Number3 = number3;
+            int sum = Number1 + Number2 + Number3;
+            return sum;
+        }
+        
+    }
+}
